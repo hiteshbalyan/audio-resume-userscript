bac383cb243a286806428f771fc3cdd819dba57c
Hitesh Balyan <hiteshbalyan@users.noreply.github.com>
2023-02-20 16:24:22 +0530

    Create Resume-Audio.user.js

---
+++ Resume-Audio.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Resume-Audio
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://hdaudiobooks.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hdaudiobooks.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var els = document.querySelectorAll('audio');
+    console.log("Running script");
+    els.forEach(el => {
+        const isPlaying = true
+        el.addEventListener('play', (event) => {
+            const srcCurrentTime = Number(window.localStorage.getItem(el.currentSrc));
+            console.log("start playing " + el.currentSrc + " from " + srcCurrentTime);
+            if(srcCurrentTime && el.currentTime < srcCurrentTime){
+                // if we got any saved time;
+                el.currentTime = srcCurrentTime;
+            }
+        })
+
+        el.addEventListener('timeupdate', (event) => {
+            window.localStorage.setItem(el.currentSrc, el.currentTime);
+        });
+
+//        el.addEventListener('seeked', ev =>{
+//            console.log(el.currentTime)
+//        })
+    })
+})();
