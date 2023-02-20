42e584e535b0dd8209e88bffd97e6db9c40ef9f8
Hitesh Balyan <hiteshbalyan@users.noreply.github.com>
2023-02-20 16:31:35 +0530

    Update Resume-Audio.user.js

--- Resume-Audio.user.js
+++ Resume-Audio.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://hdaudiobooks.com/*
+// @match        *://(hd|hq)audiobooks.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hdaudiobooks.com
 // @grant        none
 // ==/UserScript==
